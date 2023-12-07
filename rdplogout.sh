0a112f6038036964fa973aad19cc4d1724dd0268
tjscola <anthonyscola@outlook.com>
2023-12-07 18:53:35 -0500

    Add RDP logout script to fix any issues cause by GUIs

---
+++ rdplogout.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#####################################################
+#   Application Name: RDP Logout                    #
+#   Desc: This is a simple logout script for        #
+#         logging out while in an RDP session.      #
+#         Sometimes the GUI doesn't do it right.    #
+#                                                   #
+#   By: Anthony Scola                               #
+#   Last updated: 12/07/2023                        #
+#####################################################
+
+# Detect the desktop environment
+DESKTOP_ENV=$(echo $XDG_CURRENT_DESKTOP | tr '[:upper:]' '[:lower:]')
+
+case $DESKTOP_ENV in
+  gnome)
+    # GNOME, Unity
+    gnome-session-quit --logout --no-prompt
+    ;;
+  kde)
+    # KDE
+    qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+    ;;
+  xfce)
+    # XFCE
+    xfce4-session-logout --logout
+    ;;
+  lxde|lxqt)
+    # LXDE, LXQT
+    lxsession-logout
+    ;;
+  *)
+    # Fallback: attempt to use 'logout' (may not work in all cases)
+    logout
+    ;;
+esac
