115b05bb3a933efcc326e4150da5dbd91915f5f8
tjscola <anthonyscola@outlook.com>
2023-12-07 21:35:21 -0500

    Add help message to uscp.sh script

--- uscp.sh
+++ uscp.sh
@@ -12,6 +12,19 @@
 # Get the directory of the script
 SCRIPT_DIR=$(dirname "$0")
 
+# Check if the first command line argument is --help or -h
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    echo "USB Secure Copy (uscp.sh)"
+    echo "-------------------------"
+    echo "Description: This script appends the contents of an rsa.pub file to your local authorized_keys."
+    echo "Usage: ./uscp.sh [public_key_path]"
+    echo "Options:"
+    echo "  -h, --help    Show this help message and exit."
+    echo "Arguments:"
+    echo "  public_key_path    Optional. Path to the public key. If not provided, the default path ($SCRIPT_DIR/id_rsa.pub) will be used."
+    exit 0
+fi
+
 # Path to the public key on your flash drive
 # Use the first command line argument as the public key path, if provided
 PUB_KEY_PATH=${1:-"$SCRIPT_DIR/id_rsa.pub"}
