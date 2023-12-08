e723a3b1de5dcd77319b61e4ecd0c448922727c1
tjscola <anthonyscola@outlook.com>
2023-12-07 19:06:37 -0500

    Add error checking

--- uscp.sh
+++ uscp.sh
@@ -15,8 +15,20 @@ SCRIPT_DIR=$(dirname "$0")
 # Path to the public key on your flash drive
 PUB_KEY_PATH="$SCRIPT_DIR/id_rsa.pub"
 
+# Exit if the public key file doesn't exist
+if [ ! -f "$PUB_KEY_PATH" ]; then
+    echo "The id_rsa.pub file is not present in the script directory."
+    exit 1
+fi
+
 # Path to the authorized_keys file on your system
 AUTHORIZED_KEYS_PATH="$HOME/.ssh/authorized_keys"
+
+# Create the authorized_keys file if it doesn't exist
+if [ ! -f "$AUTHORIZED_KEYS_PATH" ]; then
+    touch $AUTHORIZED_KEYS_PATH
+    echo "authorized_keys file created."
+fi
 
 # Append the public key to the authorized_keys file on your system
 cat $PUB_KEY_PATH >> $AUTHORIZED_KEYS_PATH
