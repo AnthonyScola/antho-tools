14fd6d62302928a116f23254e9f143dbbb798863
tjscola <anthonyscola@outlook.com>
2023-12-07 19:02:35 -0500

    Update SSH key appending in uscp.sh

--- uscp.sh
+++ uscp.sh
@@ -6,7 +6,17 @@
 #         your authorized_keys locally.             #
 #                                                   #
 #   By: Anthony Scola                               #
-#   Last updated: 12/02/2023                        #
+#   Last updated: 12/07/2023                        #
 #####################################################
 
-cat id_rsa.pub >> ~/.ssh/authorized_keys # Uh I couldn't get this to work...
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$0")
+
+# Path to the public key on your flash drive
+PUB_KEY_PATH="$SCRIPT_DIR/id_rsa.pub"
+
+# Path to the authorized_keys file on your system
+AUTHORIZED_KEYS_PATH="$HOME/.ssh/authorized_keys"
+
+# Append the public key to the authorized_keys file on your system
+cat $PUB_KEY_PATH >> $AUTHORIZED_KEYS_PATH
